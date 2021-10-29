0de879b0f3167f1250616be7221f47bf8c1583f0
ankit <ph.official.ankit@gmail.com>
2021-10-29 14:01:32 +0530

    create feature search, filter, pagination

--- server/controllers/productController.js
+++ server/controllers/productController.js
@@ -1,6 +1,7 @@
 const Product = require("../models/productModel");
 const ErrorHandler = require("../utils/errorHandler");
 const catchAsyncErrors = require("../middleware/catchAsyncErrors");
+const ApiFeatures = require("../utils/apiFeatures");
 
 // create product -- Admin
 exports.createProduct = catchAsyncErrors(async (req, res, next) => {
@@ -63,14 +64,25 @@ exports.getProductDetails = catchAsyncErrors(async (req, res, next) => {
   res.status(200).json({
     success: true,
     product,
+    // productCount,
   });
 });
 
 // get all products
 exports.getAllProducts = catchAsyncErrors(async (req, res) => {
-  const products = await Product.find();
+  const resultPerPage = 5;
+  const productCount = Product.countDocuments();
+
+  const apiFeature = new ApiFeatures(Product.find(), req.query)
+    .search()
+    .filter()
+    .pagination(resultPerPage);
+
+  const products = await apiFeature.query;
+
   res.status(200).json({
     success: true,
     products,
+    // productCount,
   });
 });
---
+++ server/utils/apiFeatures.js
@@ -0,0 +1,57 @@
+class ApiFeatures {
+  constructor(query, queryStr) {
+    this.query = query;
+    this.queryStr = queryStr;
+  }
+
+  // search
+  search() {
+    const keyword = this.queryStr.keyword
+      ? {
+          name: {
+            $regex: this.queryStr.keyword,
+            $options: "i",
+          },
+        }
+      : {};
+
+    // console.log(keyword);
+
+    this.query = this.query.find({ ...keyword });
+
+    return this;
+  }
+
+  // filter
+  filter() {
+    const queryCopy = { ...this.queryStr };
+
+    // console.log(queryCopy);
+
+    // removing some feilds for category
+    const removeFields = ["keyword", "page", "limit"];
+    removeFields.forEach((key) => delete queryCopy[key]);
+
+    // console.log(queryCopy);
+
+    // filter for price and rating
+    let queryStr = JSON.stringify(queryCopy);
+    queryStr = queryStr.replace(/\b(gt|gte|lt|lte)\b/g, (key) => `$${key}`);
+
+    this.query = this.query.find(JSON.parse(queryStr));
+
+    return this;
+  }
+
+  // pagination
+  pagination(resultPerPage) {
+    const currentPage = Number(this.queryStr.page) || 1;
+    // product to skip
+    const skip = resultPerPage * (currentPage - 1);
+
+    this.query = this.query.limit(resultPerPage).skip(skip);
+    return this;
+  }
+}
+
+module.exports = ApiFeatures;
